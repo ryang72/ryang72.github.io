f15b1aa2d6d5ce9321753d6db95a1b18f10d5662
Richard <richbluehill@gmail.com>
2017-10-26 00:00:09 -0700

    v 2.12 added border to photo selection

--- gallery-script.js
+++ gallery-script.js
@@ -1,5 +1,10 @@
 function makeMain(but) {
     var x = document.getElementsByClassName('mainImage');
-    console.log();
+    var tot = document.getElementsByClassName('preview-element');
     x[0].src = but.value;
+    for (var i = 0; i < 20; i++) {
+        tot[i].classList.remove('preview-selection');
+    }
+    but.classList.add('preview-selection');
+    console.log('adding');
 }
